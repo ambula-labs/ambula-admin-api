d6a302e52310fb4b58d0845ac362a515631da0ab
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-19 15:43:17 +0200

    try another db connection

--- db/db.js
+++ db/db.js
@@ -6,10 +6,6 @@ const pool = createPool({
 	port: 3306,
 	user: "root",
 	database: "ambuladb",
-	authPlugins: {
-		mysql_clear_password: () => () => Buffer.from(""),
-		mysql_native_password: require("mysql/lib/auth_plugins/mysql_native_password"),
-	},
 });
 
 const query = promisify(pool.query).bind(pool);
