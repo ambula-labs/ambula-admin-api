7275d9856b697a0ef3249ee2e71473371748ccc1
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-19 10:35:36 +0200

    try another db connection

--- db/db.js
+++ db/db.js
@@ -1,12 +1,13 @@
-import { createConnection } from "mysql";
+import { createPool } from "mysql";
 import { promisify } from "util";
 
-const connection = createConnection({
-	host: "localhost",
+const pool = createPool({
+	host: "127.0.0.1",
+	port: 3306,
 	user: "root",
 	database: "ambuladb",
 });
 
-const query = promisify(connection.query).bind(connection);
+const query = promisify(pool.query).bind(pool);
 
 export default query;
