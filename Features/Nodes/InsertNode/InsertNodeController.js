5b85e49841ee3d97d269cd245a829a199f51b319
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-21 23:41:25 +0200

    ✏️ Function Name

--- Features/Nodes/InsertNode/InsertNodeController.js
+++ Features/Nodes/InsertNode/InsertNodeController.js
@@ -2,7 +2,7 @@ import handle from "./InsertNodeService.js";
 import InsertNodeRequestFactory from "./InsertNodeRequestFactory.js";
 
 async function invoke(req, reply) {
-	const insertNodeRequest = InsertNodeRequestFactory.createInsertActivityRequest(req);
+	const insertNodeRequest = InsertNodeRequestFactory.createInsertNodeRequest(req);
 	const node = await handle(insertNodeRequest);
 	reply.send(node);
 }
