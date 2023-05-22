f825e3382d1fa35b13d4dd1179d45f85127717cc
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-22 17:05:25 +0200

    🐛 Fix async

--- Facades/AnsibleFacade.js
+++ Facades/AnsibleFacade.js
@@ -1,33 +1,41 @@
 import { spawn } from "child_process";
 
-export default async function createLinode(linodeLabel) {
-	// Spawn a new child process to run the Ansible playbook
-	const ansible = spawn("ansible-playbook", ["./AnsiblePlaybooks/DeployLinode.yml", "--extra-vars", `linode_label=${linodeLabel}`]);
+export default function createLinode(linodeLabel) {
+	return new Promise((resolve, reject) => {
+		// Spawn a new child process to run the Ansible playbook
+		const ansible = spawn("ansible-playbook", ["./AnsiblePlaybooks/DeployLinode.yml", "--extra-vars", `linode_label=${linodeLabel}`]);
 
-	// Log the output of the child process to the console
-	ansible.stdout.on("data", (data) => {
-		console.log(`stdout: ${data}`);
-	});
+		// Log the output of the child process to the console
+		ansible.stdout.on("data", (data) => {
+			console.log(`stdout: ${data}`);
+		});
 
-	// Handle errors and exit code of the child process
-	ansible.on("close", (code) => {
-		console.log(`child process exited with code ${code}`);
-		return code;
+		// Handle errors and exit code of the child process
+		ansible.on("close", (code) => {
+			console.log(`child process exited with code ${code}`);
+			if (code === 0) {
+				resolve(code);
+			} else {
+				reject(new Error(`Ansible playbook failed with exit code ${code}`));
+			}
+		});
 	});
 }
 
 export async function deleteLinode(linodeLabel) {
-	// Spawn a new child process to run the Ansible playbook
-	const ansible = spawn("ansible-playbook", ["./AnsiblePlaybooks/DeleteLinode.yml", "--extra-vars", `linodeLabel=${linodeLabel}`]);
+	return new Promise((resolve, reject) => {
+		// Spawn a new child process to run the Ansible playbook
+		const ansible = spawn("ansible-playbook", ["./AnsiblePlaybooks/DeleteLinode.yml", "--extra-vars", `linodeLabel=${linodeLabel}`]);
 
-	// Log the output of the child process to the console
-	ansible.stdout.on("data", (data) => {
-		console.log(`stdout: ${data}`);
-	});
+		// Log the output of the child process to the console
+		ansible.stdout.on("data", (data) => {
+			console.log(`stdout: ${data}`);
+		});
 
-	// Handle errors and exit code of the child process
-	ansible.on("close", (code) => {
-		console.log(`child process exited with code ${code}`);
-		return code;
+		// Handle errors and exit code of the child process
+		ansible.on("close", (code) => {
+			console.log(`child process exited with code ${code}`);
+			resolve(code);
+		});
 	});
 }
