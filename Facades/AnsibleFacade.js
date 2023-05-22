4d9de65f8b8df069843c7c4ac010c78481317db6
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-22 21:47:07 +0200

    🧱 When Deploy a Linode install Git and Clone repo

--- Facades/AnsibleFacade.js
+++ Facades/AnsibleFacade.js
@@ -5,16 +5,28 @@ export default function createLinode(linodeLabel) {
 		// Spawn a new child process to run the Ansible playbook
 		const ansible = spawn("ansible-playbook", ["./AnsiblePlaybooks/DeployLinode.yml", "--extra-vars", `linode_label=${linodeLabel}`]);
 
+		let linodeIp = ""; // Variable to store the linode_ip value
+
 		// Log the output of the child process to the console
 		ansible.stdout.on("data", (data) => {
-			console.log(`stdout: ${data}`);
+			const output = data.toString();
+			console.log(`stdout: ${output}`);
+
+			// Check if the output contains the linode_ip variable
+			if (output.includes("linode_ip")) {
+				// Extract the linode_ip value from the output
+				const match = output.match(/linode_ip\": \"(.+?)\"/);
+				if (match) {
+					linodeIp = match[1];
+				}
+			}
 		});
 
 		// Handle errors and exit code of the child process
 		ansible.on("close", (code) => {
 			console.log(`child process exited with code ${code}`);
 			if (code === 0) {
-				resolve(code);
+				resolve(linodeIp); // Resolve the linode_ip value
 			} else {
 				reject(new Error(`Ansible playbook failed with exit code ${code}`));
 			}
