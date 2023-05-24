df75082f9da1d60a4427bcdc3f1e2cb698d57af6
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-24 06:36:21 +0200

    ✨ Add and Delete Nodes

--- Features/Ambula/DeleteAmbulaNode/DeleteAmbulaNodeService.js
+++ Features/Ambula/DeleteAmbulaNode/DeleteAmbulaNodeService.js
@@ -1,13 +1,13 @@
 import { deleteLinode } from "../../../Facades/AnsibleFacade.js";
 import insertActivityRequest from "../../Activity/InsertActivity/InsertActivityRequest.js";
 import insertActivityService from "../../Activity/InsertActivity/InsertActivityService.js";
-import deleteNodeService from "../../Nodes/DeleteNode/DeleteNodeService.js";
+import { deleteNode } from "../../../Repositories/NodesRepository.js";
 import { getNode } from "../../../Repositories/NodesRepository.js";
 
 async function handle(req) {
 	const node = await getNode(req.params.node_id);
 	await deleteLinode(node.name);
-	await deleteNodeService(req.params.node_id);
+	await deleteNode(req.params.node_id);
 
 	const insertActRequest = new insertActivityRequest("Deleting " + node.name + " node");
 	await insertActivityService(insertActRequest);
