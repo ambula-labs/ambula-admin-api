b7b600a77e28333538ba6f2df08731160c0eb8df
Quentin Hattstadt <quentin.hattstadt@chargemap.com>
2023-05-24 05:52:38 +0200

    Reboot Node

--- Routes/Ambula.js
+++ Routes/Ambula.js
@@ -1,14 +1,14 @@
 import startAmbulaController from "../Features/Ambula/StartAmbula/StartAmbulaController.js";
-//import restartAmbulaController from "../Features/Ambula/RestartAmbula/RestartAmbulaController.js";
+import rebootAmbulaNodeController from "../Features/Ambula/RebootAmbulaNode/RebootAmbulaNodeController.js";
 import stopAmbulaController from "../Features/Ambula/StopAmbula/StopAmbulaController.js";
 
 async function nodesRoute(fastify, options) {
 	fastify.post("/start", async (request, reply) => {
 		await startAmbulaController(request, reply);
 	});
-	// fastify.post("/restart", async (request, reply) => {
-	// 	await restartAmbulaController(request, reply);
-	// });
+	fastify.post("/reboot/:node_id", async (request, reply) => {
+		await rebootAmbulaNodeController(request, reply);
+	});
 	fastify.post("/stop", async (request, reply) => {
 		await stopAmbulaController(request, reply);
 	});
